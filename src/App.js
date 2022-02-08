0be80696b893c60b1549fbc5d9d31262b624b405
elizieb <elizieb.l3@gmail.com>
2022-02-08 09:19:56 -0300

    fix: ajust conflict responsive header;

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 // import logo from './logo.svg';
+import { useEffect } from "react";
 import "./global.css";
 import Panel from "./pages/panel";
+import ReactGA from "react-ga";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-54J42DPK9M");
+    ReactGA.pageview("Init page view");
+    // G-54J42DPK9M
+  }, []);
+
   return <Panel />;
 }
 
